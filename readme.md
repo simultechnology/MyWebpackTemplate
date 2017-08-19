a69f019c89682b23307eb8e77a61b9c5402f5543
t_ishikawa <tissi0708@gmail.com>
2017-08-19 14:43:53 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# A boilerplate in which you use webpack
+
+## Usage
+
+```
+npm install --save-dev webpack
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+let path = require('path');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: "app.bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    host: '127.0.0.1',
+    port: 9590
+  }
+};
