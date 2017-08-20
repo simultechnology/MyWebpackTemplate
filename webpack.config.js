3008ff2fbf80d07605ae14feae2aa467284d5420
t_ishikawa <tissi0708@gmail.com>
2017-08-21 08:38:31 +0900

    add sample

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 let path = require('path');
 
-module.exports = {
-  entry: './app.js',
-  output: {
-    filename: "app.bundle.js"
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './public'),
-    host: '127.0.0.1',
-    port: 9590,
-    open: true
+module.exports = [
+  // {
+  //   entry: './app.js',
+  //   output: {
+  //     filename: "app.bundle.js"
+  //   },
+  //   devServer: {
+  //     contentBase: path.resolve(__dirname, './public'),
+  //     host: '127.0.0.1',
+  //     port: 9590,
+  //     open: true
+  //   }
+  // },
+  {
+    entry: './src/js/timer.js',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: "timer.bundle.js"
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, './dist'),
+      host: '127.0.0.1',
+      port: 9591,
+      watchOptions: {
+        watch: true,
+        poll: 1000
+      },
+      open: true
+    },
   }
-};
+
+];
